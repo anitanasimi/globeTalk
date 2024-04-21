0aea40fad02f558b5651291a142efc7870c2dde0
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-04-21 15:31:43 -0700

    a route added to the new chat icon

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -1,13 +1,18 @@
 "use client"
 
+import { useRouter } from "next/navigation"
 import { Button } from "./ui/button"
 import {MessageSquarePlusIcon} from "lucide-react"
 
 
 
 function CreateChatButton() {
+    const router = useRouter()
+    const createNewChat =  async() => {
+        router.push(`/chat/abc`)
+    }
   return (
-    <Button><MessageSquarePlusIcon/></Button>
+    <Button onClick= {createNewChat} variant = {'ghost'}><MessageSquarePlusIcon/></Button>
   )
 }
 
