9acb2e4fc0f9515da552ffa1666e83e230ab595d
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-04-21 01:44:28 -0700

    fixed deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
-      <h1>Let's converse seamlessly across languages</h1>
+      <h1>Let&apos;s converse seamlessly across languages</h1>
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Logo from "./Logo";
-import { DarkModeToggle } from "./DarkModeToggle";
+import DarkModeToggle  from "./DarkModeToggle";
 
 
 function Header() {
