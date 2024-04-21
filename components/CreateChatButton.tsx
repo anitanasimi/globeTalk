a7ad943c4d473846da64883405ceeeadca069fcc
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-04-21 15:25:01 -0700

    new message icon added to the Header component

---
+++ components/CreateChatButton.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Button } from "./ui/button"
+import {MessageSquarePlusIcon} from "lucide-react"
+
+
+
+function CreateChatButton() {
+  return (
+    <Button><MessageSquarePlusIcon/></Button>
+  )
+}
+
+export default CreateChatButton
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { authOptions } from "@/auth";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { MessagesSquareIcon } from "lucide-react";
+import CreateChatButton from "./CreateChatButton";
 
 async function Header() {
   const session = await getServerSession(authOptions);
@@ -16,13 +17,17 @@ async function Header() {
         <Logo />
         <div className="flex-1 flex items-center justify-end space-x-4">
           {session ? (
+            <>
             <Link href="/chat" prefetch={false}>
               <MessagesSquareIcon className="text-black dark:text-white" />
             </Link>
+             <CreateChatButton/>
+             </>
           ) : (
             <Link href="pricing">
             Pricing
             </Link>
+           
           )}
           <DarkModeToggle />
           <UserButton session={session}/>
