895312a780465055b9a03afdcf62638e08790def
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-04-21 18:37:22 -0700

    create register page -> add checkout button -> fix the deployment issue in pricing page

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function CheckoutButton() {
+    const createCheckoutSession = async () => {
+
+    }
   return (
-    <div>CheckoutButton</div>
-  )
+      <div className='flex flex-col space-y-2'>
+
+      
+    <button onClick={()=> createCheckoutSession()} className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font semibold landing-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus:visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 ">
+      Sign Up
+    </button>
+      </div>
+  );
 }
 
 export default CheckoutButton
