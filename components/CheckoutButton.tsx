c613e76a8dea772ddb5c89c590baa9c645c4a946
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-04-28 01:14:24 -0700

    feat: Use client-side rendering for checkout session creation
    Refactored the CheckoutButton component to use client-side rendering for creating checkout sessions. This change reduces server load.

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,30 +1,56 @@
-"use client";
-
+"use client"
 import { db } from "@/firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 
 function CheckoutButton() {
   const { data: session } = useSession();
-  const [loading,setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+
+  // Define a function to create a checkout session
   const createCheckoutSession = async () => {
+    // Check if the user is authenticated
     if (!session?.user.id) return;
 
-    setLoading(true)
-const docRef = await addDoc(collection(db,'customers', session.user.id, 'checkout_session'),{
-  price:"",
-  success_url: window.location.origin,
-  cancel_url: window.location.origin
-})
+    // Set loading state to true
+    setLoading(true);
+
+    // Add a document to the "checkout_session" collection for the user
+    const docRef = await addDoc(
+      collection(db, "customers", session.user.id, "checkout_session"),
+      {
+        price: "price_1PASFt2KtuVaNhXhCthVq0xq",
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      }
+    );
+
+    // Listen for changes to the document and handle the response
+    return onSnapshot(docRef, (snap) => {
+      const data = snap.data();
+      const url = data?.url;
+      const error = data?.error;
+      if (error) {
+        // Display an alert if there is an error
+        alert(`An error occurred: ${error.message}`);
+        setLoading(false);
+      }
+      if (url) {
+        // Redirect to the checkout URL if available
+        window.location.assign(url);
+        setLoading(false);
+      }
+    });
   };
+
   return (
     <div className="flex flex-col space-y-2">
       <button
         onClick={() => createCheckoutSession()}
-        className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font semibold landing-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus:visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 "
+        className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font semibold landing-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus:visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80"
       >
-        Sign Up
+       {loading ? "loading...": "Sign Up"}
       </button>
     </div>
   );
