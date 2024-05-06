5c0794e91d64c6c203784ce1e7fe0cb4a71bde10
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-05-05 21:19:06 -0700

    creatChatButton component add

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -1,19 +1,79 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation"
-import { Button } from "./ui/button"
-import {MessageSquarePlusIcon} from "lucide-react"
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { MessageSquarePlusIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useToast } from "./ui/use-toast";
+import { useSubscriptionStore } from "@/store/store";
+import LoadingSpinner from "./LoadingSpinner";
+import { v4 as uuidv4 } from "uuid";
+import { Timestamp } from "firebase-admin/firestore";
+import { serverTimestamp, setDoc } from "firebase/firestore";
+import { addChatRef } from "@/lib/converters/ChatMembers";
 
+function CreateChatButton({ isLarge }: { isLarge?: boolean }) {
+  const { data: session } = useSession();
 
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+  const subscription = useSubscriptionStore((state) => state.subscription);
 
-function CreateChatButton() {
-    const router = useRouter()
-    const createNewChat =  async() => {
-        router.push(`/chat/abc`)
-    }
+  const createNewChat = async () => {
+    if (!session?.user.id) return;
+    setLoading(true);
+    toast({
+      title: "creating a new chat...",
+      description: "Hold tight while we create your new chat",
+      duration: 3000,
+    });
+
+    const chatId = uuidv4();
+
+    await setDoc(addChatRef(chatId, session.user.id), {
+      userId: session.user.id!,
+      email: session.user.email!,
+      timestamp: serverTimestamp(),
+      isAdmin: true,
+      chatId: chatId,
+      image: session.user.image || "",
+    }).then(() => {
+      toast({
+        title: "success",
+        description: "your chat has been created!",
+        className: "bg-green-600 text-white",
+        duration: 2000,
+      });
+      router.push(`/chat/${chatId}`);
+    }).catch(()=> {
+      toast({
+        title:"Error",
+        description:"there was an error creating your chat",
+        variant:"destructive"
+      })
+    }).finally ( ()=> {
+      setLoading(false)
+    });
+
+    router.push(`/chat/abc`);
+  };
+
+  if (isLarge)
+    return (
+      <div>
+        <Button variant={"default"} onClick={createNewChat}>
+          {loading ? <LoadingSpinner /> : "Create a new chat"}
+        </Button>
+      </div>
+    );
+
   return (
-    <Button onClick= {createNewChat} variant = {'ghost'}><MessageSquarePlusIcon/></Button>
-  )
+    <Button onClick={createNewChat} variant={"ghost"}>
+      <MessageSquarePlusIcon />
+    </Button>
+  );
 }
 
-export default CreateChatButton
+export default CreateChatButton;
