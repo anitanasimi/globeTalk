162b2d6ceacd2c3572db552fd59a4a10eddb7fad
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-05-07 14:42:13 -0700

    update checkout button component to fix the error not going to the checkout page

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -7,22 +7,18 @@ import { useState } from "react";
 
 import { useSubscriptionStore } from "@/store/store";
 
+
+
 import LoadingSpinner from "./LoadingSpinner";
 import ManageAccountButton from "./ManageAccountButton";
 
 function CheckoutButton() {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
-  const subscription = useSubscriptionStore((state) => state.subscription);
-
-  const isLoadingSubscription = subscription === undefined;
-  const isSubscribed =
-    subscription?.status === "active" && subscription?.role === "pro";
 
   const createCheckoutSession = async () => {
     if (!session?.user.id) return;
 
-    // push a document into firestore db
     setLoading(true);
     const docRef = await addDoc(
       collection(db, "customers", session.user.id, "checkout_sessions"),
@@ -33,17 +29,13 @@ function CheckoutButton() {
       }
     );
 
-    // ... stripe extension on firebase will create a checkout session
     return onSnapshot(docRef, (snap) => {
       const data = snap.data();
       const url = data?.url;
       const error = data?.error;
 
       if (error) {
-        /*
-        Show an error to your customer and inspect your cloud functions logs in the firebase console.
-        */
-        alert(`An error occured : ${error.message}`);
+        alert(`An error occurred: ${error.message}`);
         setLoading(false);
       }
 
@@ -53,22 +45,11 @@ function CheckoutButton() {
       }
     });
   };
+
   return (
     <div className="flex flex-col space-y-2">
-      {/* If subscribed show me the user is subscribed */}
-
-      {isSubscribed && (
-        <>
-          <hr className="mt-5" />
-          <p className="pt-5 text-center text-xs text-indigo-600">
-            You are subscribed to PRO
-          </p>
-        </>
-      )}
-      <div className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white   disabled:cursor-default">
-        {isSubscribed ? (
-          <ManageAccountButton />
-        ) : isLoadingSubscription || loading ? (
+      <div className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default">
+        {loading ? (
           <LoadingSpinner />
         ) : (
           <button onClick={() => createCheckoutSession()}>Sign Up</button>
